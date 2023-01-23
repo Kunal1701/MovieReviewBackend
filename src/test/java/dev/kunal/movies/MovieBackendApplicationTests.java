04f07124dd8b90fe3075cad42a808b41a980131b
Kunal1701 <80157692+Kunal1701@users.noreply.github.com>
2023-01-23 17:52:53 +0530

    first commit

---
+++ src/test/java/dev/kunal/movies/MovieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kunal.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
